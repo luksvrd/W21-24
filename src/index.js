18ebc677b5b4c2ebf99066fabb0187c718f488b3
luksvrd <ljvy9b@gmail.com>
2022-11-14 19:25:13 -0800

    pass in items from topic repos

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
  * If it's a 'default' export, no need to use curly braces.
  */
 import apiService from "./services/api.service";
+import renderService from "./services/render.service";
 
 document.querySelectorAll("button").forEach((button) => {
   button.classList.add("button");
@@ -15,16 +16,19 @@ document.querySelector("form").addEventListener("submit", async (event) => {
 
   const data = await apiService.getReposByUsername(username);
 
-  console.log(data);
+  renderService.renderRepos(username, data);
 });
 
-document.getElementById("topics").addEventListener("click", (event) => {
+document.getElementById("topics").addEventListener("click", async (event) => {
   // With EVENT BUBBLING, we can listen to the parent element and get the target element from the event object.
   // lets make sure we only listen to the buttons.
   // this is synonymous with textContent & innerText
   // this code logs the inner text on a button that was clicked
   if (event.target.tagName === "BUTTON") {
-    const topic = event.target.innerText;
+    const topic = event.target.innerText.toLowerCase();
+    // github API will send an 'items' array with the repo
     const data = await apiService.getReposByTopic(topic);
+
+    renderService.renderRepos(topic, data.items);
   }
 });
